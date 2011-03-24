a875e20cfbceda572ed7c0f8d6044bacd61a3d8a
Samuel Kadolph <samuel@kadolph.com>
2011-03-24 10:14:33 -0400

    Initial Commit

---
+++ git-password/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  git-password
+//
+//  Created by Kadolph, Samuel on 11-03-24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
